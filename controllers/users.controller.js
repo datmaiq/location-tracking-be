f52a8df0225eb8d575453cca5fc0ae16abb714a6
Dat Mai <datmaiq@gmail.com>
2024-06-22 14:30:40 +0700

    chore: remove redundant new local var

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,3 @@
-const multer = require("multer");
-const { GridFsStorage } = require("multer-gridfs-storage");
-const Grid = require("gridfs-stream");
-const methodOverride = require("method-override");
 const User = require("../model/User.model");
 const mongoose = require("mongoose");
 
@@ -18,68 +14,65 @@ conn.once("open", () => {
 
 const getUser = async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.params.username });
+    const user = await User.findOne({username: req.params.username});
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({message: "User not found"});
     }
     res.json(user);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({message: error.message});
   }
 };
 
 // Endpoint to upload an image
 const uploadUserImage = async (req, res) => {
   const userId = req.params.userId;
-  const { avatar, coverPhoto } = req.files;
+  const {avatar, coverPhoto} = req.files;
 
   const updateData = {};
   if (avatar && avatar.length > 0) {
     const avatarId = avatar[0].id;
-    const avatarUrl = `${req.protocol}://${req.get(
+    updateData.profileBanner = `${req.protocol}://${req.get(
       "host"
     )}/users/file/${avatarId}`;
-    updateData.profileBanner = avatarUrl;
     updateData.profileBannerId = avatarId;
   }
   if (coverPhoto && coverPhoto.length > 0) {
     const coverPhotoId = coverPhoto[0].id;
-    const coverPhotoUrl = `${req.protocol}://${req.get(
+    updateData.coverPhoto = `${req.protocol}://${req.get(
       "host"
     )}/users/file/${coverPhotoId}`;
-
-    updateData.coverPhoto = coverPhotoUrl;
     updateData.coverPhotoId = coverPhotoId;
   }
 
   try {
     const user = await User.findByIdAndUpdate(
       userId,
-      { $set: updateData },
-      { new: true }
+      {$set: updateData},
+      {new: true}
     );
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({message: "User not found"});
     }
-    res.status(201).json({ files: req.files, user });
+    res.status(201).json({files: req.files, user});
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({message: error.message});
   }
 };
 
 // Endpoint to serve images by ID
 const getImageById = (req, res) => {
   const fileId = new mongoose.Types.ObjectId(req.params.id);
-  gfs.find({ _id: fileId }).toArray((err, files) => {
+  gfs.find({_id: fileId}).toArray((err, files) => {
     if (err) {
-      return res.status(500).json({ message: err.message });
+      return res.status(500).json({message: err.message});
     }
     if (!files || files.length === 0) {
-      return res.status(404).json({ message: "No file found" });
+      return res.status(404).json({message: "No file found"});
     }
 
     gfs.openDownloadStream(fileId).pipe(res);
   });
 };
 
-module.exports = { getUser, uploadUserImage, getImageById };
+module.exports = {getUser, uploadUserImage, getImageById};
